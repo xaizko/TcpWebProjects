f1bfa9ac8561a1cb285dc74c6cc198ec2df25013
xaizko <tonytran177013@gmail.com>
2025-05-20 21:56:47 -0400

    Added sample test code - returns the header of a website (yahoo)

--- tcpClient.c
+++ tcpClient.c
@@ -1,16 +1,23 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define IP "76.76.21.21" //Whatever ip address you want
+#define IP "98.137.11.164" //Whatever ip address you want
 #define PORT 80 //standard http PORT
 
 int main() {
     int s;
     struct sockaddr_in sock; //a structure for handling internet addresses
+    char buf[512];
+    char *data;
+
+    //change this if you want different information
+    data = "HEAD / HTTP/1.0\r\n\r\n";
 
     //create the socket
     s = socket(AF_INET, SOCK_STREAM, 0);
@@ -32,4 +39,13 @@ int main() {
 
 	return -1;
     }
+
+    //copy data
+    write(s, data, strlen(data));
+    memset(buf, 0, 512);
+    read(s, buf, 511);
+    close(s);
+    //print it!
+    printf("\n%s\n", buf);
 }
+
