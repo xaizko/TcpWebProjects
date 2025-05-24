caf88549224639f6beda0759d22d47f23a554911
xaizko <tonytran177013@gmail.com>
2025-05-23 23:28:19 -0400

    Added png file upload, working decently should organize code

--- webserver.c
+++ webserver.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define LISTENADDR "127.0.0.1"
 
@@ -15,6 +17,12 @@ struct httpRequest {
 
 };
 
+struct sFile {
+    char filename[64];
+    char *fileContent;
+    int size;
+}; typedef struct sFile File;
+
 //function headers
 int cli_accept(int s);
 void cli_conn(int s, int c);
@@ -22,6 +30,8 @@ struct httpRequest *parse_http(char *str);
 char *cli_read(int c); 
 void http_headers(int c, int code);
 void http_response(int c, char *contentType, char *data);
+int sendfile(int c, char *contentType, File *file);
+File *readFile(char *fileName);
 
 int main(int argc, char *argv[]) {
     int s, c;
@@ -102,6 +112,8 @@ void cli_conn(int s, int c) {
     struct httpRequest *req;
     char *p;
     char *response;
+    char str[96];
+    File *f;
 
     p = cli_read(c);
     if (!p) {
@@ -114,9 +126,25 @@ void cli_conn(int s, int c) {
     if (!req) {
 	fprintf(stderr, "failed to parse\n");
     }
-
+    if (!strcmp(req->method, "GET") && !strncmp(req->url, "/img/", 5)) {
+	memset(str, 0, 96);
+	snprintf(str, 95, ".%s", req->url);
+	f = readFile(str);
+	if (!f) {
+	    response = "File not found";
+	    http_headers(c, 404);
+	    http_response(c, "text/plain", response);
+	} else {
+	    http_headers(c, 200);
+	    if (!sendfile(c, "image/png", f)) {
+		response = "HTTP server error";
+		http_headers(c, 500);
+		http_response(c, "text/plain", response);
+	    }
+	}
+    }
     if (!strcmp(req->method, "GET") && !strcmp(req->url, "/app/webpage")) {
-	response = "<html>Hello world</html>";
+	response = "<html><img src='/img/kitty.png' alt='image' /></html>";
 	http_headers(c, 200);
 	http_response(c, "text/html", response);
     } else {
@@ -130,6 +158,77 @@ void cli_conn(int s, int c) {
     return;
 }
 
+File *readFile(char *fileName) {
+    char buf[512];
+    char *p;
+    int n, x, fd;
+    File *f;
+
+    fd = open(fileName, O_RDONLY);
+    if (fd < 0) {
+	return 0;
+    }
+
+    f = malloc(sizeof(struct sFile));
+    if (!f) {
+	close(fd);
+	return 0;
+    }
+
+    strncpy(f->filename, fileName, 63);
+    f->fileContent = malloc(512);
+
+    x = 0; // num of bytes read
+    while (1) {
+	memset(buf, 0, 512);
+	n = read(fd, buf, 512);
+	if (!n) {
+	    break;
+	} else if (x == -1) {
+	    close(fd);
+	    free(f->fileContent);
+	    free(f);
+	    return 0;
+	}
+
+	strncpy(buf, (f->fileContent)+x, n);
+	x += n;
+	f->fileContent = realloc(f->fileContent, (512+x));
+    }
+
+    f->size = x;
+    close(fd);
+    return f;
+}
+
+int sendfile(int c, char *contentType, File *file) {
+    char buf[512];
+    int n, x;
+    char *p;
+
+    if (!file) {
+	return 0;
+    }
+
+    snprintf(buf, 511,
+	     "Content-Type: %s\n"
+	     "Content-Length: %d\n"
+	     "\r\n",
+	     contentType, file->size);
+    write(c, buf, strlen(buf));
+
+    n = file->size;
+    p = file->fileContent;
+    while (n > 0) {
+	int x = write(c, p, (n < 512) ? n : 512);
+	if (x < 1) {
+	    return 0;
+	}
+	n -= x;
+	p += x;
+    }
+    return 1;
+}
 
 struct httpRequest *parse_http(char *str) {
     struct httpRequest *req;
@@ -176,10 +275,10 @@ void http_headers(int c, int code) {
     int n;
 
     snprintf(buf, 511, 
-	"HTTP/1.0 %d OK\n"
-	"Server: webserver.c\n"
-	"Content-Language: en\n"
-	"Expires: -1\n", code);
+	"HTTP/1.0 %d OK\r\n"
+	"Server: webserver.c\r\n"
+	"Content-Language: en\r\n"
+	"Expires: -1\r\n", code);
 
     n = strlen(buf);
     write(c, buf, n);
